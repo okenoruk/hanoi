0df9945ae7e32ff89023b8899c667b7abd0fc9fc
dyhaz <muhammad-11@fst.unair.ac.id>
2021-06-17 23:01:26 +0700

    first commit

---
+++ public/build/app.js
@@ -0,0 +1,18 @@
+/// <reference path="types/phaser.d.ts"/>
+var SimpleGame = /** @class */ (function () {
+    function SimpleGame() {
+        this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', { preload: this.preload, create: this.create });
+    }
+    SimpleGame.prototype.preload = function () {
+        this.game.load.image('logo', 'img/sky.png');
+    };
+    SimpleGame.prototype.create = function () {
+        var logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo');
+        logo.anchor.setTo(0.5, 0.5);
+    };
+    return SimpleGame;
+}());
+window.onload = function () {
+    var game = new SimpleGame();
+};
+//# sourceMappingURL=app.js.map
