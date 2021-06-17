8441d3470fb09f7ca44e529c733df566edf9fb9b
dyhaz <muhammad-11@fst.unair.ac.id>
2021-06-17 23:47:57 +0700

    feat: added floor

--- public/build/app.js
+++ public/build/app.js
@@ -1,14 +1,24 @@
 /// <reference path="types/phaser.d.ts"/>
 var SimpleGame = /** @class */ (function () {
-    function SimpleGame() {
-        this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', { preload: this.preload, create: this.create });
+    function SimpleGame(disks) {
+        if (disks === void 0) { disks = 3; }
+        this.disks = disks;
+        this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', { preload: this.preload, create: this.create, render: this.render });
     }
     SimpleGame.prototype.preload = function () {
-        this.game.load.image('logo', 'img/sky.png');
+        this.game.load.image('bg', 'img/sky.png');
     };
     SimpleGame.prototype.create = function () {
-        var logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo');
-        logo.anchor.setTo(0.5, 0.5);
+        console.log('create');
+        var bg = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'bg');
+        bg.anchor.setTo(0.5, 0.5);
+        this.floor = new Phaser.Rectangle(0, 500, 800, 50);
+    };
+    SimpleGame.prototype.render = function () {
+        this.game.debug.rectangle(this.floor, '#ff00aa');
+        for (var i = 1; i <= this.disks; i++) {
+            // draw disks
+        }
     };
     return SimpleGame;
 }());
--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -2,25 +2,39 @@
 
 class SimpleGame {
 
-  constructor() {
-    this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', { preload: this.preload, create: this.create });
+  constructor(disks = 3) {
+    this.disks = disks;
+    this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content',
+      { preload: this.preload, create: this.create, render: this.render });
   }
 
   game: Phaser.Game;
+  disks: number;
+
+  floor: Phaser.Rectangle;
 
   preload() {
-    this.game.load.image('logo', 'img/sky.png');
+    this.game.load.image('bg', 'img/sky.png');
   }
 
   create() {
-    var logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo');
-    logo.anchor.setTo(0.5, 0.5);
+    console.log('create');
+
+    const bg = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'bg');
+    bg.anchor.setTo(0.5, 0.5);
+
+    this.floor = new Phaser.Rectangle(0, 500, 800, 50);
   }
 
+  render() {
+    this.game.debug.rectangle(this.floor, '#708090');
+
+    for (let i = 1 ; i <= this.disks ; i ++) {
+      // draw disks
+    }
+  }
 }
 
 window.onload = () => {
-
-  var game = new SimpleGame();
-
+  const game = new SimpleGame();
 };
